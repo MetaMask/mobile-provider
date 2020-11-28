92b815e660f5f59f6aa28404093acf0f2523cfc9
Erik Marks <rekmarks@protonmail.com>
2020-11-28 09:42:42 -0800

    Remove web3

--- src/content-script/wrapper.js
+++ src/content-script/wrapper.js
@@ -2,14 +2,8 @@
 const fs = require('fs')
 const path = require('path')
 const inpageContent = fs.readFileSync(path.join(__dirname, '..', '..', '/', 'dist', 'inpage.js')).toString()
-const web3Js = fs.readFileSync(path.join(__dirname, '..', '..', '/', 'node_modules', 'web3', 'dist', 'web3.min.js')).toString()
-const setProviderContent = fs.readFileSync(path.join(__dirname, 'setProvider.js')).toString()
 
-const code = `
-    const inpageBundle = ${JSON.stringify(inpageContent)} + \n
-                         ${JSON.stringify(web3Js)} + \n
-                         ${JSON.stringify(setProviderContent)};
-`
+const code = `const inpageBundle = ${JSON.stringify(inpageContent)}`
 
 fs.writeFileSync(path.join(__dirname, 'inpage-bundle.js'), code, 'ascii', () => {
   console.log('content-script.js generated succesfully')
