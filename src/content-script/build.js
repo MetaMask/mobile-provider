7133610573cb4c937570034e7b348c2164edfc87
Erik Marks <rekmarks@protonmail.com>
2020-11-30 14:07:06 -0800

    Minify all of output bundle

--- src/content-script/build.js
+++ src/content-script/build.js
@@ -5,8 +5,8 @@ const distPath = path.join(__dirname, '..', '..', '/', 'dist')
 
 const inpageContent = fs.readFileSync(path.join(distPath, 'inpage-content.js')).toString()
 
+// wrap the inpage content in a variable declaration
 const code = `const inpageBundle = ${JSON.stringify(inpageContent)}`
 
-fs.writeFileSync(path.join(distPath, 'inpage-bundle.js'), code, 'ascii', () => {
-  console.log('content-script.js generated succesfully')
-})
+fs.writeFileSync(path.join(distPath, 'inpage-bundle.js'), code, 'ascii')
+console.log('content-script.js generated succesfully')
