e9700457a25146aa611795c4abdb7746f143aab0
Jiexi Luan <jiexiluan@gmail.com>
2023-09-28 17:09:17 -0700

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,27 @@ const config = {
   },
 
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        },
+      }
+    ],
+  },
+  resolve: {
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+    }
+  },
 };
 
 module.exports = (_env, argv) => {
